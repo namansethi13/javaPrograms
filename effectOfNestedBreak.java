d0b2eeceb5264cc34b32fa37f4e20db8466f8520
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-04-05 14:56:53 +0530

    effect of nested break

--- effectOfNestedBreak.java
+++ effectOfNestedBreak.java
@@ -1,5 +1,13 @@
 //Demonstrate the effect of nested break.
 
 public class effectOfNestedBreak {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if(i == 5){
+                System.out.println("i is "+ i +" breaking out of the loop");
+                break;
+            }
+            System.out.println("i is "+ i);
+        }
+    }
 }
